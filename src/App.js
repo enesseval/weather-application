442d804e511b3531328f847978ac268ce7519f55
Enes Seval <enesseval@outlook.com>
2022-09-11 13:45:24 +0300

    Weather-Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from "./components/search/Search";
+import "./App.css";
+import { SearchContextProvider } from "./context/SearchContext";
+import { WeatherContextProvider } from "./context/WeatherContext";
+import Weather from "./components/weather/Weather";
+import Forecast from "./components/forecast/Forecast";
+import { DayContextProvider } from "./context/DayContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <SearchContextProvider>
+        <Search />
+        <WeatherContextProvider>
+          <DayContextProvider>
+            <Weather />
+          </DayContextProvider>
+        </WeatherContextProvider>
+      </SearchContextProvider>
     </div>
   );
 }
